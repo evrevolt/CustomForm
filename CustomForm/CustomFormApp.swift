999a69fc295993b378f34ff9222e55ab0f532dbf
Gennady Vedernikov <revoltlight@icloud.com>
2022-11-18 21:28:03 +0300

    Initial Commit

---
+++ CustomForm/CustomFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomFormApp.swift
+//  CustomForm
+//
+//  Created by Геннадий Ведерников on 18.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
